ecd34ebb6a55bf5461c37e900ba1d6f856068446
shuyao <shuyao.guo@tu-berlin.de>
2024-06-06 15:23:00 +0200

    Added workflows

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "eflips-opt"
+copyright = "MPM TU Berlin"
+author = "Shuyao Guo"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "autoapi.extension"]
+autodoc_typehints = "description"
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for AutoAPI -----------------------------------------------------
+
+autoapi_add_toctree_entry = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+
+# -- Options for autoapi -----------------------------------------------------
+autoapi_dirs = ["../eflips/"]
